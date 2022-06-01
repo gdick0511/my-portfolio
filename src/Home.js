78e2e7db43d80237f762f1046e58c863c7eadf39
garrettdick <garrettd0511@gmail.com>
2022-06-01 11:27:48 -0500

    updated logo and favicon

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import Forest from './images/Forest.png'
+import GDLogo from './images/GDLogo.png'
 
 function Home(){
 
     return(
         
         <div>
-            <img className='Forest' src={Forest}></img>
+            <img className='GDLogo' src={GDLogo}></img>
             <p className='Home'>Welcome to my portfolio and all things me. This is a space where projects, technologies, passion and goals all coalesce. </p>
         </div>
     )
